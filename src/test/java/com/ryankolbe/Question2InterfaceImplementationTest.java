c4b2b71a28ba88789fcd3e8dcb981b103f57e201
RyanKolbe <31966635+RyanKolbe@users.noreply.github.com>
2019-03-03 23:54:36 +0200

    final changes

--- src/test/java/com/ryankolbe/Question2InterfaceImplementationTest.java
+++ src/test/java/com/ryankolbe/Question2InterfaceImplementationTest.java
@@ -1,30 +1,33 @@
 package com.ryankolbe;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Question2InterfaceImplementationTest {
 
-    private Question2Interface question2Interface;
+    private Question2Interface1 question2Interface1;
+    private Question2Interface2 question2Interface2;
 
-    @Before
+    @BeforeClass
     public void setUp() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Question3ConfigClass.class);
-        question2Interface = (Question2Interface) applicationContext.getBean("Ch2");
+        ApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(Question3ConfigClass.class);
+        question2Interface1 = (Question2Interface1) applicationContext1.getBean("Ch2_Interface1");
+        ApplicationContext applicationContext2 = new AnnotationConfigApplicationContext(Question3ConfigClass.class);
+        question2Interface2 = (Question2Interface2) applicationContext2.getBean("Ch2_Interface2");
     }
 
     @Test
     public void calculateArea() {
-        int result = question2Interface.calculateArea(2);
+        int result = question2Interface1.calculateArea(2);
         Assert.assertEquals(12, result);
     }
 
     @Test
     public void calculatePerimeter() {
-        int result = question2Interface.calculatePerimeter(2);
+        int result = question2Interface2.calculatePerimeter(2);
         Assert.assertEquals(12, result);
     }
 }
